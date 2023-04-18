5bbe39243639e7a0f495b96f2aba5f3a216080ce
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-04-18 16:33:24 -0300

    Service layer, component registration

--- src/main/java/com/example/xbrain/controller/SellerController.java
+++ src/main/java/com/example/xbrain/controller/SellerController.java
@@ -2,19 +2,33 @@ package com.example.xbrain.controller;
 
 import com.example.xbrain.entities.Seller;
 
+import com.example.xbrain.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sellers")
 public class SellerController {
 
+    @Autowired
+    private SellerService service;
+
     @GetMapping
-    public ResponseEntity<Seller> findAll() {
-        Seller s = new Seller(1L, "Lucas");
-        return ResponseEntity.ok().body(s);
+    public ResponseEntity<List<Seller>> findAll() {
+        List<Seller> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Seller> findById(@PathVariable Long id) {
+        Seller obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
 
---
+++ src/main/java/com/example/xbrain/service/SellerService.java
@@ -0,0 +1,25 @@
+package com.example.xbrain.service;
+
+import com.example.xbrain.entities.Seller;
+import com.example.xbrain.repository.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SellerService {
+
+    @Autowired
+    private SellerRepository repository;
+
+    public List<Seller> findAll() {
+        return repository.findAll();
+    }
+
+    public Seller findById(Long id) {
+        Optional<Seller> obj = repository.findById(id);
+        return obj.get();
+    }
+}

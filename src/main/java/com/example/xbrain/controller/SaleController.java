126970cd09a67c16491004f451eff68bd84ce651
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-04-25 10:44:00 -0300

    Sale put, update and delete

--- src/main/java/com/example/xbrain/controller/SaleController.java
+++ src/main/java/com/example/xbrain/controller/SaleController.java
@@ -2,12 +2,15 @@ package com.example.xbrain.controller;
 
 import com.example.xbrain.dto.SellerDTO;
 import com.example.xbrain.entities.Sale;
+import com.example.xbrain.entities.Seller;
 import com.example.xbrain.service.SaleService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.time.Instant;
 import java.util.List;
 
@@ -30,11 +33,27 @@ public class SaleController {
         Sale obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
-//    @GetMapping(value = "/between")
-//    public ResponseEntity<List<SellerDTO>> findAllByDateBetween(@RequestParam Instant startDate, @RequestParam Instant endDate) {
-//        List<SellerDTO> list = service.findBetween(startDate, endDate);
-//        return ResponseEntity.ok().body(list);
-//    }
+
+    @PostMapping
+    public ResponseEntity<Sale> insert(@RequestBody Sale obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Sale> update(@PathVariable Long id, @RequestBody Sale obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
     @GetMapping(value = "/between")
     public ResponseEntity<List<Object[]>> findAllByDateBetween(@RequestParam Instant startDate, @RequestParam Instant endDate) {
         List<Object[]> list = service.findDateBetween(startDate, endDate);
--- src/main/java/com/example/xbrain/service/SaleService.java
+++ src/main/java/com/example/xbrain/service/SaleService.java
@@ -1,8 +1,14 @@
 package com.example.xbrain.service;
 
 import com.example.xbrain.entities.Sale;
+import com.example.xbrain.entities.Seller;
 import com.example.xbrain.repository.SaleRepository;
+import com.example.xbrain.service.exception.DatabaseException;
+import com.example.xbrain.service.exception.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -24,9 +30,35 @@ public class SaleService {
         return obj.get();
     }
 
-//    public List<SellerDTO> findBetween(@RequestParam Instant startDate, @RequestParam Instant endDate) {
-//
-//    }
+    public Sale insert(Sale obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Sale update(Long id, Sale obj) {
+        try {
+            Sale entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    public void updateData(Sale entity, Sale obj) {
+        entity.setDate(obj.getDate());
+        entity.setPrice(obj.getPrice());
+        entity.setSeller(obj.getSeller());
+    }
 
     public List<Object[]> findDateBetween(Instant startDate, Instant endDate){
         return repository.findDateBetween(startDate,endDate);
